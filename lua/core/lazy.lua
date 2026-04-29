ee0f6302f5ce2065776e34cc6dbfd240befc65d5
yzk <1076992525@qq.com>
2026-04-29 18:12:23 +0800

    add lualine

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -17,3 +17,18 @@ require("lazy").setup({
         { import = "plugins" };
     }
 })
+require("lualine").setup({
+  options = {
+    theme = "auto",
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { "filename" },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+})
+
